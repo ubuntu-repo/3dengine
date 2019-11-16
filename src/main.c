585e186097bde7711f57a20d893c79d5624fdbcf
Gustavo Pezzi <gustavocp@gmail.com>
2019-11-16 21:48:57 +0000

    Added raw line and triangle rastering functions.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <math.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL2_gfxPrimitives.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// Define two macros to return the max and min between two values
+///////////////////////////////////////////////////////////////////////////////
+#define MAX(x,y) ((x) >= (y)) ? (x) : (y)
+#define MIN(x,y) ((x) <= (y)) ? (x) : (y)
 
 ///////////////////////////////////////////////////////////////////////////////
 // Define constants for FPS and game loop frame time
@@ -91,6 +97,12 @@ SDL_Renderer* renderer = NULL;
 unsigned int last_frame_time = 0;
 unsigned window_width = 800;
 unsigned window_height = 600;
+
+///////////////////////////////////////////////////////////////////////////////
+// Declare a color buffer array and a texture that will be used to display it
+///////////////////////////////////////////////////////////////////////////////
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Functions to rotate 3D points in X, Y, and Z
@@ -192,9 +204,15 @@ void process_input(void) {
 // Setup function to initialize game objects and game state
 ///////////////////////////////////////////////////////////////////////////////
 void setup(void) {
-    cube_rotation.x = 0.0;
-    cube_rotation.y = 0.0;
-    cube_rotation.z = 0.0;
+    color_buffer = (uint32_t *) malloc(sizeof(uint32_t) * (uint32_t)window_width * (uint32_t) window_height);
+
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -233,6 +251,157 @@ void update(void) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// Renders the color buffer array in a texture and displays it
+///////////////////////////////////////////////////////////////////////////////
+void render_color_buffer() {
+    SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer, (int)((uint32_t)window_width * sizeof(uint32_t)));
+    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color) {
+    for (int x = 0; x < window_width; x++)
+        for (int y = 0; y < window_height; y++)
+            color_buffer[(window_width * y) + x] = color;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Render drawing primitives
+///////////////////////////////////////////////////////////////////////////////
+void draw_pixel(int x, int y, uint32_t color) {
+    color_buffer[(window_width * y) + x] = color;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Render a line using the inneficient DDA line drawing algorithm
+///////////////////////////////////////////////////////////////////////////////
+void dda_line(int x1, int y1, int x2, int y2, uint32_t color) {
+    int delta_x = (x2 - x1);
+    int delta_y = (y2 - y1);
+
+    int step = abs(delta_x) >= abs(delta_y) ? abs(delta_x) : abs(delta_y);
+
+    // Algorithm is naive and slow as it uses floating point for step increment
+    float x_inc = delta_x / (float) step;
+    float y_inc = delta_y / (float) step;
+
+    float x = x1;
+    float y = y1;
+    for (int i = 0; i <= step; i++) {
+        draw_pixel((int)x, (int)y, color);
+        x += x_inc;
+        y += y_inc;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Render a line using the Bresenham line drawing algorithm
+///////////////////////////////////////////////////////////////////////////////
+void plot_line_low(int x0, int y0, int x1, int y1, uint32_t color) {
+    int dx = (x1 - x0);
+    int dy = (y1 - y0);
+    int yi = 1;
+
+    if (dy < 0) {
+        yi = -1;
+        dy = -dy;
+    }
+
+    int D = (2 * dy) - dx;
+    int y = y0;
+
+    for (int x = x0; x <= x1; ++x) {
+        draw_pixel(x, y, color);
+        if (D > 0) {
+            y = y + yi;
+            D = D - (2 * dx);
+        }
+        D = D + 2*dy;
+    }
+}
+
+void plot_line_high(int x0, int y0, int x1, int y1, uint32_t color) {
+    int dx = (x1 - x0);
+    int dy = (y1 - y0);
+    int xi = 1;
+
+    if (dx < 0) {
+        xi = -1;
+        dx = -dx;
+    }
+
+    int D = (2 * dx) - dy;
+    int x = x0;
+
+    for (int y = y0; y <= y1; ++y) {
+        draw_pixel(x, y, color);
+        if (D > 0) {
+            x = x + xi;
+            D = D - (2 * dy);
+        }
+        D = D + (2 * dx);
+    }
+}
+
+void bresenham_line(int x0, int y0, int x1, int y1, uint32_t color) {
+    if (abs(y1 - y0) < abs(x1 - x0)) {
+        if (x0 > x1)
+            plot_line_low(x1, y1, x0, y0, color);
+        else
+            plot_line_low(x0, y0, x1, y1, color);
+    } else {
+        if (y0 > y1)
+            plot_line_high(x1, y1, x0, y0, color);
+        else
+            plot_line_high(x0, y0, x1, y1, color);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Draw an unfilled triangle using raw line calls
+///////////////////////////////////////////////////////////////////////////////
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+    bresenham_line(x0, y0, x1, y1, color);
+    bresenham_line(x1, y1, x2, y2, color);
+    bresenham_line(x2, y2, x0, y0, color);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Perpendicular dot product between two 2D vectors
+///////////////////////////////////////////////////////////////////////////////
+float cross_z(point2d a, point2d b, point2d c) {
+    // flip y because of the coordinate system
+    return (b.x - a.x) * -(c.y - a.y) - -(b.y - a.y) * (c.x - a.x);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Draw an unfilled triangle using raw line calls
+///////////////////////////////////////////////////////////////////////////////
+void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+    // get the bounding box of the triangle
+    int max_x = MAX(x0, MAX(x1, x2));
+    int min_x = MIN(x0, MIN(x1, x2));
+    int max_y = MAX(y0, MAX(y1, y2));
+    int min_y = MIN(y0, MIN(y1, y2));
+
+    point2d v0 = { .x = x0, .y = y0 };
+    point2d v1 = { .x = x1, .y = y1 };
+    point2d v2 = { .x = x2, .y = y2 };
+
+    for (int x = min_x; x < max_x; x++) {
+        for (int y = min_y; y < max_y; y++) {
+            // sample from the center of the pixel, not the top-left corner
+            point2d p = { .x = x + 0.5, .y = y + 0.5 };
+            // if the point is not inside our polygon, skip fragment
+            if (cross_z(v1, v2, p) < 0 || cross_z(v2, v0, p) < 0 || cross_z(v0, v1, p) < 0) {
+                continue;
+            } else {
+                draw_pixel(x, y, color);
+            }
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // Render function to draw game objects in the display
 ///////////////////////////////////////////////////////////////////////////////
 void render(void) {
@@ -245,17 +414,36 @@ void render(void) {
         point2d point_a = projected_points[cube_triangles[i].a];
         point2d point_b = projected_points[cube_triangles[i].b];
         point2d point_c = projected_points[cube_triangles[i].c];
+        uint32_t triangle_color = cube_triangles[i].color;
 
-        // Draw a trigon (triangle) from point a, b, and c
-        // Also translates/moves them to the center of the screen
-        filledTrigonColor(
-            renderer,
-            point_a.x + (window_width / 2), point_a.y + (window_height / 2),
-            point_b.x + (window_width / 2), point_b.y + (window_height / 2),
-            point_c.x + (window_width / 2), point_c.y + (window_height / 2),
-            cube_triangles[i].color
+        // Draw a filled triangle and translates it to the screen center
+        draw_filled_triangle(
+            point_a.x + (window_width / 2),
+            point_a.y + (window_height / 2),
+            point_b.x + (window_width / 2),
+            point_b.y + (window_height / 2),
+            point_c.x + (window_width / 2),
+            point_c.y + (window_height / 2),
+            triangle_color
+        );
+
+        // Draw triangle lines and translate them to the screen center
+        draw_triangle(
+            point_a.x + (window_width / 2),
+            point_a.y + (window_height / 2),
+            point_b.x + (window_width / 2),
+            point_b.y + (window_height / 2),
+            point_c.x + (window_width / 2),
+            point_c.y + (window_height / 2),
+            0x00000000
         );
     }
+
+    // Render the color buffer using a SDL texture
+    render_color_buffer();
+
+    // Clear the colorBuffer before the reder of the next frame
+    clear_color_buffer(0xFF000000);
 
     SDL_RenderPresent(renderer);
 }
@@ -264,6 +452,7 @@ void render(void) {
 // Function to destroy renderer, window, and exit SDL
 ///////////////////////////////////////////////////////////////////////////////
 void destroy_window(void) {
+    free(color_buffer);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
